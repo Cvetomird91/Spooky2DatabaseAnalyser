35e45166a230ebe7af1e9ce639a7652f63824c33
Cvetomir Denchev <cvetomirdenchev@gmail.com>
2018-04-04 15:06:38 +0300

    started OOP refactoring of main logic

--- include/Spooky2DatabaseAnalyser.h
+++ include/Spooky2DatabaseAnalyser.h
@@ -1,16 +1,23 @@
 #ifndef SPOOKY2DATABASEANALYSER_H
 #define SPOOKY2DATABASEANALYSER_H
 
-/*
+#include <string>
+#include <vector>
+
+#include <boost/filesystem.hpp>
+
 class Spooky2DatabaseAnalyser
 {
     public:
         Spooky2DatabaseAnalyser();
-
-    protected:
+        Spooky2DatabaseAnalyser(std::string, std::string);
+        void setTxtFiles(boost::filesystem::path);
 
     private:
+        std::string m_Path;
+        std::string m_Search_string;
+        std::vector<boost::filesystem::path> txt_files;
+
 };
-*/
 
 #endif // SPOOKY2DATABASEANALYSER_H
--- src/Spooky2DatabaseAnalyser.cpp
+++ src/Spooky2DatabaseAnalyser.cpp
@@ -1,14 +1,17 @@
 #include "Spooky2DatabaseAnalyser.h"
+
 #include <string>
+#include <vector>
 
-class Spooky2DatabaseAnalyser
-{
-    public:
-        Spooky2DatabaseAnalyser();
+#include <boost/filesystem.hpp>
 
-    protected:
-        std::string path;
-        std::string search_string;
 
-    private:
-};
+Spooky2DatabaseAnalyser::Spooky2DatabaseAnalyser() {}
+
+void Spooky2DatabaseAnalyser::setTxtFiles(boost::filesystem::path p) {
+
+}
+
+Spooky2DatabaseAnalyser::Spooky2DatabaseAnalyser(std::string path, std::string search_string) : m_Path(path),  m_Search_string(search_string) {
+    this->setTxtFiles(m_Path);
+}
