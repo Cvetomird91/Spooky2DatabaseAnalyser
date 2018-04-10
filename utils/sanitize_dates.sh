258d04d5ec7946f518700c2a74dd204e92fc3a1e
Cvetomir Denchev <cvetomirdenchev@gmail.com>
2018-04-10 16:11:06 +0300

    script to normalize dates to a single format

--- utils/sanitize_dates.sh
+++ utils/sanitize_dates.sh
@@ -1,12 +1,35 @@
 #!/usr/bin/env bash
 
 DB_PATH=$1
-# declare -p $PATH
 TXT_FILES="${DB_PATH}/*.txt"
 
+# :"
+#     0505
+#     02.05.2017
+#     14.05.17
+#     02052017
+# "
+
 for file in ${TXT_FILES}; do
-	 #echo ${file};
-	 new_file_name=$( echo ${file} | sed "s/\.17/\.2017/" );
-	 echo ${new_file_name};
-	# echo ${file} | sed "s/\.17/\.2017/";
+	new_file_name=$( echo ${file} | sed "s/\.17/\.2017/" );
+ 	echo ${new_file_name};
+	mv -v "${file}" "${new_file_name}";
+done
+
+for file in ${TXT_FILES}; do
+	#occurence=$(ack -ic "(\d{4})(?=[a-z])" "${file}");
+	#echo "${file}" | perl -pe 's/(?<=[A-Za-z])(\d{2})(\d{2})(?=[A-Za-z])/$2.$3.2017/';
+	#echo "${file}:";
+	new_file_name=$( echo "${file}" | perl -pe 's/(?<=[A-Za-z])(\d{2})(\d{2})(?=[\.A-Za-z])/$1.$2.2017/');
+	echo "${file}" | perl -pe 's/(?<=[A-Za-z])(\d{2})(\d{2})(?=[\.A-Za-z])/$1.$2.2017/'
+	mv -v "${file}" "${new_file_name}";
+	#echo "${occurence}";
+	#if [ $occurence -gt 1 ]; then
+	#	echo "${file}";
+	#fi
+done
+
+for file in ${TXT_FILES}; do
+	new_file_name=$( echo "${file}" | perl -pe 's/(\d{2})(\d{2})(\d{4})/$1.$2.$3/' );
+	mv -v "${file}" "${new_file_name}";
 done
